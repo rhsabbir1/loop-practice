c6f4bd82d11f6d85cb3d3d9833fcc3c486b227fd
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-27 22:22:48 +0600

    while loop

---
+++ while-loop.js
@@ -0,0 +1,21 @@
+// if(confitons){
+//     console.log('..........')
+// } Its one time run
+
+// while loop 
+
+// structure 
+while(condition){
+    // loop body
+    // value +;
+}
+
+
+var studentCount = 0;
+
+while(studentCount <= 10){
+    console.log('Present')
+    console.log(studentCount)
+    studentCount++;
+}
+
