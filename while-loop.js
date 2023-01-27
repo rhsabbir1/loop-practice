6b5fe8ef78ed7df4552702b3a081acf299d6dbc2
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-27 22:37:45 +0600

    eve and odd number

--- while-loop.js
+++ while-loop.js
@@ -5,17 +5,45 @@
 // while loop 
 
 // structure 
-while(condition){
-    // loop body
-    // value +;
-}
+
+// loop variable 
+
+// while(condition){
+//     loop body
+//     value +;
+// }
 
 
 var studentCount = 0;
 
 while(studentCount <= 10){
-    console.log('Present')
-    console.log(studentCount)
+    // console.log('Present')
+    // console.log(studentCount)
     studentCount++;
 }
 
+// Display 1 to 20 
+var number = 1;
+
+while(number <= 20){
+    // console.log(number)
+    number++;
+}
+
+// Even number 
+
+var number = 2;
+
+while(number <= 20){
+    // console.log(number)
+    number+= 2;
+}
+
+// Odd number
+
+var number = 1;
+
+while(number <= 20){
+    console.log(number)
+    number+= 2;
+}
