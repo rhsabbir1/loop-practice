c499df1415c058256e50103857bbf55cda682527
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-27 21:59:33 +0600

    recave prevous module

---
+++ recave.js
@@ -0,0 +1,67 @@
+// variable
+
+// numaric variable 
+var penPrice = 15;
+var bottolePrice = 150;
+var sunglassPrice = 200;
+
+
+// String type variable 
+var elderSister = 'Ananna';
+var bottleColour = 'Orang';
+var speicalOne = 'Moriom';
+
+// Boolean type vsrisble
+var isExpencibe = true;
+var isPass = true;
+
+// Js array 
+var friendsName = ['Ananna' , 'Moriom', 'Atik','Anu','Nur','Shadin']
+
+var speicalOne = friendsName[2];
+friendsName[3] = 'Antik';
+console.log(speicalOne)
+
+var nurIndex = friendsName.indexOf('Nur')
+console.log(nurIndex)
+
+// conditions 
+
+// if statement 
+if(conditions){
+
+}
+else{
+
+}
+
+// nasted conditions
+if(conditions){
+    if(consditions){
+
+    }
+    else if(conditions){
+
+    }
+    else{
+
+    }
+}
+
+else if(conditions){
+
+}
+else{
+
+}
+
+
+
+
+
+
+
+
+
+
+
