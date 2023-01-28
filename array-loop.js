64fb22ba139182303ffa4ad787e05743b94aa958
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-28 22:30:37 +0600

    array-loop

---
+++ array-loop.js
@@ -0,0 +1,27 @@
+for (var i = 0 ; i < 7 ; i++){
+    // console.log(i)
+}
+
+// Target : Display every elements of Array 
+var numbers = [12, 22, 33, 44, 55, 66, 77];
+
+for(var i=0; i<7; i++){
+    var number = numbers[i];
+    console.log(number)
+}
+
+var friends =['Antik', 'Mahabur', 'Nur', 'Atik','Mahim'];
+
+for(var i=0; i<5; i++){
+    var friend = friends[i];
+    // console.log(friend)
+}
+
+var items = ['Laptop', 'Mouse','NoteBook', 'Books','Mumput','Looking Glass'];
+
+for(var i=0; i< items.length; i++){
+    var item = items[i];
+    console.log(item)
+    // console.log(i)
+}
+
--- for.js
+++ for.js
@@ -6,23 +6,23 @@ while(rostgiven <= 10){
 }
 
 // for loop 
-for(rostgiven =0; rostgiven <= 10; rostgiven++){
+for( var rostgiven =0; rostgiven <= 10; rostgiven++){
     console.log('something')
 }
 
 //Usual system
 
-for(i = 0; i <= 10; i++){
+for(var i = 0; i <= 10; i++){
     console.log(i)
 }
 
 // Even number usuing for loop 
-for(i = 2;  i <= 10; i+=2){
+for(var i = 2;  i <= 10; i+=2){
     console.log(i)
 }
 
 
 // Odd number 
-for(i = 1 ; i <= 10; i+=2){
+for(var i = 1 ; i <= 10; i+=2){
     console.log(i)
 }
